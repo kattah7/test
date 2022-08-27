9b47ebdf918b013ea684a97294b27165cb5a95d2
Kattah <kattahpro@gmail.com>
2022-08-27 10:21:47 +0900

    xd

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
+app.use(express.static("src/public"));
 
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.redirect('https://youtu.be/d1YBv2mWll0')
+});
+
+app.get(`/kekw`, (req: Request, res: Response, next: NextFunction) => {
+    res.render('kekw.ejs');
 });
 
 app.listen('4000', () => {
